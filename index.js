cf149cccccb66571b9668425ef9b7ae1ca1ca4dd
Fatenkh7 <khodorfaten5@gmail.com>
2023-04-12 15:30:45 +0300

    crud done

--- index.js
+++ index.js
@@ -2,31 +2,47 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
-import createError from "http-errors"
+import createError from "http-errors";
 import cookieParser from "cookie-parser";
-dotenv.config;
-const PORT = process.env.PORT;
-await connectDB();
-const app = new express();
+import adminRoutes from "./routes/admin.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-app.listen(
-  PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`)
-);
+
+app.use("/admin", adminRoutes);
 
 // create and error object,catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
 // error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).send({
     success: false,
     message: err.message,
   });
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`);
+  });
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
