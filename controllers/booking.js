bfdc732e091cb41b1d29f3435c3cc13a20daa63b
Fatenkh7 <khodorfaten5@gmail.com>
2023-04-13 13:34:16 +0300

    booking crud done

---
+++ controllers/booking.js
@@ -0,0 +1,93 @@
+import bookingModel from "../models/Booking.js";
+
+//get all the booking
+export async function getAll(req, res, next) {
+  try {
+    const response = await bookingModel.find({});
+    return res.status(200).send({ success: true, response });
+  } catch (err) {
+    return next(err);
+  }
+}
+
+//get the booking by ID
+export async function getById(req, res, next) {
+  try {
+    const { ID } = req.params;
+    const admin = await bookingModel.findById(ID);
+    if (!admin) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Booking not found" });
+    }
+    res.status(200).send({ success: true, admin });
+  } catch (error) {
+    next(error);
+  }
+}
+
+//add booking
+export async function addBooking(req, res, next) {
+  try {
+    const { User_id, Package_id, Partner_id, TypeTravel_id, price, currency } =
+      req.body;
+
+    const newBooking = new bookingModel({
+      User_id,
+      Package_id,
+      Partner_id,
+      TypeTravel_id,
+      price,
+      currency,
+    });
+
+    await newBooking.save();
+
+    res
+      .status(201)
+      .json({ message: "Booking created successfully", newBooking });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+//Update the booking
+export async function editBookingById(req, res) {
+  try {
+    let filter = { _id: req.params.ID };
+    let update = req.body;
+
+    const updateBooking = await bookingModel.findOneAndUpdate(filter, update, {
+      //for save it in the database
+      new: true,
+    });
+    res
+      .status(200)
+      .json({ message: "Update successfully", data: updateBooking });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
+}
+
+//Delete an admin
+export async function deleteBookingById(req, res, next) {
+  try {
+    const removeBooking = await bookingModel.findOneAndDelete({
+      _id: req.params.ID,
+    });
+    res
+      .status(200)
+      .json({ data: removeBooking, message: "This Booking has been deleted" });
+  } catch (err) {
+    res.status(404).json({ message: err });
+  }
+}
+
+const controller = {
+  addBooking,
+  getAll,
+  deleteBookingById,
+  getById,
+  editBookingById,
+};
+export default controller;
--- models/Booking.js
+++ models/Booking.js
@@ -7,25 +7,30 @@ const BookingSchema = Schema(
       ref: "User",
     },
     Package_id: {
-        type: Schema.Types.ObjectId,
-        ref: "Package",
-      },
-      Partner_id: {
-        type: Schema.Types.ObjectId,
-        ref: "Partner",
-      },
-      TypeTravel_id: {
-        type: Schema.Types.ObjectId,
-        ref: "TypeTravel",
-      },
+      type: Schema.Types.ObjectId,
+      ref: "Package",
+    },
+    Partner_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Partner",
+    },
+    TypeTravel_id: {
+      type: Schema.Types.ObjectId,
+      ref: "TypeTravel",
+    },
     price: {
-        type: Schema.Types.Decimal128,
+      type: Schema.Types.Decimal128,
       default: false,
+      required: [true, "Please enter the price"],
+    },
+    currency: {
+      type: String,
+      required: [true, "Please enter the currency name"],
     },
   },
   {
     collection: "Booking",
-    timestamps: true
+    timestamps: true,
   }
 );
 const Model = model("Booking", BookingSchema);
