dd93569c8bac27a8394865455d8738c1e51aac87
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-04-18 09:22:35 +0300

    authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,38 +2,46 @@ import jwt from "jsonwebtoken";
 // import { model } from "mongoose";
 import adminModel from "../models/Admin.js";
 import userModel from "../models/user.js"
-const config=process.env
-const  authen = async (role=[]) => {
-let token
-token=req.headers.Authorization["bearer"]
-if(!token){
-return res.status(401).send("Access Denied")
-}
-try{
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    if(role.includes("superAdmin")||role.includes("admin")){
-        const admin= await adminModel.find({userName:req.body.userName})
-        let adminRole= admin.isSuper? "superAdmin": "admin";
-        if(role.includes(adminRole) &&decoded){
-            next()
+export const authorize=(role = []) =>{
+  return [
+ 
+        (req, res, next) => {
+            try{
+            let token=req.body.Authorization||"none"
+            let decoded=jwt.verify(token, process.env.JWT_SECRET);
+            if(!decoded){
+                return res.status(401).json({ message: 'Unauthorized' });
+            }}catch(error){return res.status(401).json({ message: 'Unauthorized' });}
+            if(role.includes("superAdmin")||role.includes("admin")){
+                let admin;
+                adminModel.find({userName:req.body.userName}).then(
+                    function(success){admin= success}, function(reject){admin= "none"}
+                )
+                if(admin==="none"){return res.status(401).send({message: "Access Denied"})}
+                let adminRole;
+                admin[0].isSuper? adminRole="superAdmin": adminRole="admin";
+                if(!role.includes(adminRole)){
+                   return res.status(401).send({message: "Access Denied"})
+                }
+                
+            }
+            if(role.includes("user")){
+                let user;
+                try{
+                userModel.find({_id: req.params.ID}).then(function(success){
+                    user= success
+                    console.log(user)
+                }, function(reject){user= "none"})
+               // let userRole=user.email||false
+                if(user==="none"){
+                    return res.status(401).send({message: "Access Denied"})
+                
+            }}catch(error){return res.status(400).send({err:error, data:user, message:"batata"})}} 
+            next();
+ 
         }
-        else res.status(401).send({message: "Access Denied"})  
-    }
-    if(role.includes("user")){
-        const admin= await userModel.find({email:req.body.email})
-        let userRole=admin.email? true:false
-        if(userRole && decoded){
-            next()
-        }
-        else res.status(401).send({message: "Access Denied"})  
-    }
-    
-     
-}
-catch(err){
-    console.log(req)
-    return res.status(400).send({message: err.message})
-}
-};
-
-export default authen
+ 
+    ];
+ 
+ }
+ export default authorize
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import userControllers from "../controllers/userControllers.js";
+import auth from "../middleware/auth.js";
 router.post("/add", userControllers.createUser);
 router.get("/", userControllers.getAllUser);
-router.get("/:ID", userControllers.getUserByParam);
+router.get("/:ID",auth(["user"]), userControllers.getUserByParam);
 router.put("/:ID", userControllers.updateUserById);
 router.delete("/:ID", userControllers.deleteUserById);
 router.post("/login", userControllers.login, userControllers.deleteUserById);
