ad1fdee959d6491bda6ebf5f8d6d63174d985f72
Nabigha Mogharbel <nabighamogharbel@gmail.com>
2023-05-05 18:14:52 +0300

    removing middleware

--- routes/package.js
+++ routes/package.js
@@ -2,9 +2,14 @@ import express from 'express';
 import auth from "../middleware/auth.js";
 const router = express.Router();
 import packageControllers from "../controllers/package.js"
-router.get("/", packageControllers.getAllPackages);
-router.get("/:ID",auth(["superAdmin", "admin"]), packageControllers.getPackageById);
+/*router.get("/", packageControllers.getAllPackages);
+router.get("/:ID", packageControllers.getPackageById);
 router.post("/add",auth(["superAdmin", "admin"]), packageControllers.addPackage);
 router.patch("/:ID",auth(["superAdmin", "admin"]), packageControllers.updatePackageById);
-router.delete("/:ID",auth(["superAdmin", "admin"]), packageControllers.deletePackage);
+router.delete("/:ID",auth(["superAdmin", "admin"]), packageControllers.deletePackage);*/
+router.get("/", packageControllers.getAllPackages);
+router.get("/:ID", packageControllers.getPackageById);
+router.post("/add", packageControllers.addPackage);
+router.patch("/:ID", packageControllers.updatePackageById);
+router.delete("/:ID", packageControllers.deletePackage);
 export default router;
